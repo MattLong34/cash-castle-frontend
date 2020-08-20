c9a4f05ba56dbc1df32d9907519d58ad1765713f
MattLong34 <mdlong34@gmail.com>
2020-08-20 09:53:30 -0600

    add debt calculator functionality

--- js/debt.js
+++ js/debt.js
@@ -1,5 +1,21 @@
-monthlyPayment = (Principal * IntRate) / ( 1 - (1 + IntRate)^months )
+const debtForm = document.querySelector('#debt-form')
+debtForm.addEventListener('submit', (event)=> submitForm(event))
 
-mP = (10000 * .03) / ( 1 - (1 + .03)^36 )
+const principal = document.getElementById("loan-amount")
+const rate = document.getElementById("interest-rate")
+const months = document.getElementById("number-of-months")
+const userMonthlyPayment = document.getElementById("monthly-payment")
+
+console.log((principal.value * rate.value) / ( 1 - (Math.pow((1 + rate.value), -months.value))))
 
 
+function submitForm(event){
+    event.preventDefault()
+    // updateChart()
+    calculateMonthlyPayment()
+}
+
+function calculateMonthlyPayment(){
+    monthlyPayment = (principal.value * (rate.value/1000)) / ( 1 - (Math.pow((1 + (rate.value/1000)), -months.value)))
+    userMonthlyPayment.textContent = `Monthly Payment: $${ parseFloat(monthlyPayment).toFixed(2)}`
+}
