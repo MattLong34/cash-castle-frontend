67171f7e9a0d135b8ecf5a17f656572d78d70db1
MattLong34 <mdlong34@gmail.com>
2020-08-20 14:36:43 -0600

    added investing page contribution functionality

--- js/investing.js
+++ js/investing.js
@@ -5,19 +5,8 @@ const principal = document.getElementById("starting-amount")
 const n = 12
 const years = document.getElementById("number-of-years")
 const rate = document.getElementById("rate-of-return")
-const result = document.getElementById("result")
+const contribution = document.getElementById("monthly-contribution")
 const userTotal = document.getElementById("total-display")
-
-// function calculate() {
-//     // The equation is A = p * [[1 + (r/n)] ^ nt]
-//     A = (p.value * Math.pow((1 + (r / (n * 100))), (n * t)));
-  
-//     // toFixed is used for rounding the amount with two decimal places.
-//     const totalAmount = result.innerHTML = "The total amount is " + A.toFixed(2);
-  
-//     const interestAmount = result.innerHTML += "<br> The interest is " + (A.toFixed(2) - p).toFixed(2);
-//   }
-
   
 var investingChart = new Chart(document.getElementById("investingChart"), {
     type: 'line',
@@ -48,8 +37,7 @@ function submitForm(event){
 }
 
 function updateChart() {
-    // investingChart.data.datasets[0].data = [p.value,totalAmount];
-    investingTotal = (principal.value * Math.pow((1 + (rate.value / (n * 100))), (n * years.value))).toFixed(2)
+    investingTotal = ((principal.value * Math.pow((1 + (rate.value / (n * 100))), (n * years.value))) + (contribution.value * Math.pow((1 + (rate.value / (n * 100))), (n * years.value)) - contribution.value)/(rate.value/(n*100))).toFixed(2)
 
     let annualArray = []
     // for (i = years.value; i >= 0; i--) {
@@ -71,12 +59,14 @@ function updateChart() {
 }
 
 function calculateUserTotal(){
-    investingTotal = (principal.value * Math.pow((1 + (rate.value / (n * 100))), (n * years.value))).toFixed(2)
+    investingTotal = (principal.value * Math.pow((1 + (rate.value / (n * 100))), (n * years.value))) + (contribution.value * Math.pow((1 + (rate.value / (n * 100))), (n * years.value)) - contribution.value)/(rate.value/(n*100))
+
     userTotal.textContent = `Ending Balance: $${parseFloat(investingTotal).toFixed(2)}`
 }
 
 function totalColor(){
-    investingTotal = (principal.value * Math.pow((1 + (rate.value / (n * 100))), (n * years.value))).toFixed(2)
+    investingTotal = (principal.value * Math.pow((1 + (rate.value / (n * 100))), (n * years.value))) + (contribution.value * Math.pow((1 + (rate.value / (n * 100))), (n * years.value)) - contribution.value)/(rate.value/(n*100))
+
 
     if (investingTotal < 0) {
         userTotal.style.background = "#da4f7a";
@@ -98,48 +88,24 @@ function totalColor(){
 
 
 
+// investingTotal = (principal.value * Math.pow((1 + (rate.value / (n * 100))), (n * years.value))).toFixed(2)
+
+// investingTotalWithContributions = compounded growth of principal + compounded growth of contributions
+
+function investingTotalWithContributions(){
+    newTotal = (principal.value * Math.pow((1 + (rate.value / (n * 100))), (n * years.value))) + (contribution.value * Math.pow((1 + (rate.value / (n * 100))), (n * years.value)) - contribution.value)/(rate.value/(n*100))
+    console.log(newTotal.toFixed(2))
+}
 
 
 
 
 
 
-// function floor(val)
-// {
-//   var newval=100*val;
-//   newval = Math.round(newval);
-//   var dollars = Math.floor(newval/100);
-//   var cents = newval - dollars * 100;
-//   if (cents == 0) {var centst = new String("00");}
-//   if (cents < 10) {var centst = "0" + String(cents);}
-//   if (cents > 9) {var centst = new String(cents);}
-//   return (dollars + "." + centst);
-// }
 
-// function dosum()
-// {
-//   var mi = document.temps.IR.value / 1200;
-//   var base = Number(document.temps.IB.value);
-//   document.temps.FA.value = "";
-//   var pp = 0;
-//   var yr = 0;
-//   var mc = Number(document.temps.MC.value);
-//   var yrs = Number(document.temps.YR.value);
-//   var tc = floor(base + 12 * yrs * mc);
-//   for (i=0; i< yrs * 12; i++)
-//   {
-//     base = base * (1 + mi) + mc;
-//     pp++;
-//     if (pp == 12)
-//     {
-//       yr++;
-//       if (yr < 10) { sp = " "; } else { sp = ""; }
-//       document.temps.FA.value = 
-//         document.temps.FA.value + "\n" + sp + yr + " : " + floor(base);
-//       pp = 0;
-//     }
-//   }
-//   document.temps.FB.value = floor(base);
-//   document.temps.TC.value = floor(tc)
-//   document.temps.TE.value = floor(base - tc)
-// }
+
+
+
+
+
+
